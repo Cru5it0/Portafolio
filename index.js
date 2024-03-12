03dc136ae6c277c37d1815d9bfb2933c1845bca0
Isaac Cervantes(UST <233566@ust.com>
2024-03-12 10:49:32 -0600

    cd1. section of skills compled

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+document.getElementById('btn-copy').addEventListener('click', function() {
+    var emailText = document.getElementById('input-email').innerText;
+    var tempInput = document.createElement('input').value = emailText;
+
+    document.body.appendChild(tempInput);
+    tempInput.ariaSelected();
+    document.execCommand('copy');
+    document.body.removeChild('tempInput');
+    alert('Copied email: ' + emailText);
+});
+
+
