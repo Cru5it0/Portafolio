dd4b5b54006f50347940386317326e19933e33cd
Cru5it0 <cruzisaacarandaervantes@gmail.com>
2024-03-14 14:28:23 -0600

    cd2. contact section complit

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
 'use strict';
 
-document.getElementById('btn-copy').addEventListener('click', function() {
-    var emailText = document.getElementById('input-email').innerText;
-    var tempInput = document.createElement('input').value = emailText;
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there elements matvhing the selector and if the requested instance exists
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
 
-    document.body.appendChild(tempInput);
-    tempInput.ariaSelected();
-    document.execCommand('copy');
-    document.body.removeChild('tempInput');
-    alert('Copied email: ' + emailText);
+const link1 = document.getElementById('link1');
+// const link2 = document.getElementById('link2');
+const link3 = document.getElementById('link3');
+const link4 = document.getElementById('link4');
+
+link1.addEventListener('click', () => {
+    scrollToElement('.content');
 });
 
+// link2.addEventListener('click', () => {
+//     scrollToElement('.projects');
+// });
+
+link3.addEventListener('click', () => {
+    scrollToElement('.skills');
+});
+
+link4.addEventListener('click', () => {
+    scrollToElement('.contact');
+});
+
+// Send Email Modal
+// Obtener elementos del DOM
+var modal = document.getElementById("modal");
+var openModalBtn = document.getElementById("openModalBtn");
+var closeModalBtn = document.getElementsByClassName("close")[0];
+
+// Función para abrir el modal
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+closeModalBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cerrar el modal si el usuario hace clic fuera de él
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
